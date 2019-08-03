9152ad94f9cbdc9fa28655023e91fbaa2a7f7179
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2019-08-03 21:09:00 +0200

    chore: add base SwiftUI project

---
+++ AroundMe/AroundMe/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AroundMe
+//
+//  Created by Vladimir Abramichev on 03/08/2019.
+//  Copyright © 2019 Vladimir Abramichev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
